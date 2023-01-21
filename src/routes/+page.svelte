af4c99520fe980e8d8d16d40c06406b25e3bd974
Tobias Wutz <tobiaswutz22@gmail.com>
2023-01-21 01:26:20 +0100

    forms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Select from '../lib/components/Select.svelte';
+	let current: number = 1;
+</script>
+
+<div class="bg-green-900 p-12 flex flex-col gap-4 w-full sm:flex-row">
+	<div class="w-full">
+		<Select bind:current choices={[1, 2, 3, 4, 5]} />
+		<Select bind:current choices={[1, 2, 3, 4, 5]} />
+		<Select bind:current choices={[1, 2, 3, 4, 5]} />
+		<Select bind:current choices={[1, 2, 3, 4, 5]} />
+		<Select bind:current choices={[1, 2, 3, 4, 5]} />
+	</div>
+	<div class="w-full">
+		<Select bind:current choices={[1, 2, 3, 4, 5]} />
+		<Select bind:current choices={[1, 2, 3, 4, 5]} />
+		<Select bind:current choices={[1, 2, 3, 4, 5]} />
+		<Select bind:current choices={[1, 2, 3, 4, 5]} />
+		<Select bind:current choices={[1, 2, 3, 4, 5]} />
+	</div>
+</div>
+
+<div class="bg-green-900 p-12 flex flex-col gap-4 w-full sm:flex-row">
+	<input type="search" />
+</div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,11 @@
-<h1>Hello</h1>
+<script lang="ts">
+  import Select from "../../lib/components/Select.svelte";
+  let current: number = 1;
+
+</script>
+
+
+<div class="bg-red-500 p-12">
+
+  <Select bind:current choices={[1, 1, 1]} />
+</div>
